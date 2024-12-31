d51d8b586643b88939cff9a4f27a64cc0ebef530
69JINX <avimations39228@gmail.com>
2024-12-31 21:06:50 +0530

    mongodb package comment

--- 007 - MongoDB Compass/index.js
+++ 007 - MongoDB Compass/index.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb'); // npm i mongodb
 
 const dbName = 'temp_db'
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017'; // url got from mongoDB compass
 const client = new MongoClient(url);
 
 // Connecting to Database
